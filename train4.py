7076a1f3eb42f3257340ddc9ba7c2b83008f5796
Tangtao <49675059+Trent-tangtao@users.noreply.github.com>
2019-05-30 09:26:34 +0800

    Create train4.py

---
+++ train4.py
@@ -0,0 +1,173 @@
+# set the matplotlib backend so figures can be saved in the background
+import matplotlib
+
+matplotlib.use("Agg")
+
+# import the necessary packages
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.image import img_to_array
+from keras.utils import to_categorical
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+from imutils import paths
+import random
+import cv2
+import os
+import gc
+import sys
+from keras import backend as K
+import tensorflow as tf
+
+sys.path.append('..')
+
+from net.lenet import LeNet
+from net.VGG16 import VGG_16
+from net.Unet import unet
+from net.resnet import ResnetBuilder
+from net.inceptionV4 import inception_v4
+from net.SEresnet import SEResNeXt
+
+
+def args_parse():
+    # construct the argument parse and parse the arguments  /home/tang/Desktop/imageclass/data
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-dtest", "--dataset_test", default="data/data/test",
+                    help="path to input dataset_test")
+    ap.add_argument("-dtrain", "--dataset_train", default="data/data/train",
+                    help="path to input dataset_train")
+    ap.add_argument("-m", "--model", default="./model.h5",
+                    help="path to output model")
+    ap.add_argument("-p", "--plot", type=str, default="plot.png",
+                    help="path to output accuracy/loss plot")
+    args = vars(ap.parse_args())
+    return args
+
+
+# initialize the number of epochs to train for, initial learning rate,
+# and batch size
+EPOCHS = 10
+INIT_LR = 1e-3
+BS = 32
+CLASS_NUM = 258  # remember to add 1
+norm_size = 224  # 224 299
+
+
+def get_img(imagePaths):
+    data = []
+    labels = []
+    # loop over the input images
+    for imagePath in imagePaths:
+        image = cv2.imread(imagePath)
+        image = cv2.resize(image, (norm_size, norm_size))
+        image = img_to_array(image)
+        data.append(image)
+
+        # extract the class label from the image path and update the
+        # labels list
+        label = int((imagePath.split(os.path.sep)[-2]).split('.')[0])
+        labels.append(label)
+        gc.collect()
+
+    # scale the raw pixel intensities to the range [0, 1]
+    data = np.array(data, dtype="float") / 255.0
+    labels = np.array(labels)
+    print(data.shape)
+    # convert the labels from integers to vectors
+    labels = to_categorical(labels, num_classes=CLASS_NUM)
+    return data, labels
+
+
+def load_data(path):
+    imagePaths = sorted(list(paths.list_images(path)))
+    random.seed(42)
+    random.shuffle(imagePaths)
+    while 1:
+        for i in range(0, len(imagePaths), BS):
+            data, labels = get_img(imagePaths[i:i + BS])
+            yield data, labels
+
+
+def train(args):
+    # initialize the model
+    print("[INFO] compiling model...")
+    
+     
+    os.environ["CUDA_VISIBLE_DEVICES"] = '5'
+
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    sess = tf.Session(config=config)
+    K.set_session(sess)
+
+    # 224 224
+    #model = VGG_16(width=norm_size, height=norm_size, depth=3, classes=CLASS_NUM)
+
+    # 32 32
+    # model = LeNet.build(width=norm_size, height=norm_size, depth=3, classes=CLASS_NUM)
+
+    # 224 224
+    # model = unet(width=norm_size, height=norm_size, depth=3, classes=CLASS_NUM)
+
+    # 224 224 Input Shape is  3 x 224 x 224 (th)
+    model = ResnetBuilder.build_resnet_18((3, norm_size, norm_size), CLASS_NUM)
+
+    # 299 299 Input Shape is 299 x 299 x 3 (th) or 3 x 299 x 299 (th)
+    # model = inception_v4(CLASS_NUM, 0.5, None, True)
+
+    # size size 3
+    # model = SEResNeXt(norm_size, CLASS_NUM).model
+
+    if os.path.exists('my_model_weights.h5'):
+        print("[INFO] import model weights...")
+        model.load_weights('my_model_weights.h5')
+    
+    try:
+        opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
+        model.compile(loss="categorical_crossentropy", optimizer=opt,
+                      metrics=["accuracy"])
+            
+        # train the network
+        print("[INFO] training network...")
+        H = model.fit_generator(load_data(args["dataset_train"]),
+                        validation_data=load_data(args["dataset_test"]),  steps_per_epoch=1000,
+                        epochs=EPOCHS, verbose=1, validation_steps=80)
+
+    except KeyboardInterrupt:
+        print("[INFO] Interrupted...")
+    
+    except Exception as e:
+        print(e)
+
+    else:
+        # plot the training loss and accuracy
+        plt.style.use("ggplot")
+        plt.figure()
+        N = int(H.history["loss"].__len__())
+        # N = EPOCHS
+        plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
+        plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
+        plt.plot(np.arange(0, N), H.history["acc"], label="train_acc")
+        plt.plot(np.arange(0, N), H.history["val_acc"], label="val_acc")
+        plt.title("Training Loss and Accuracy on image classifier")
+        plt.xlabel("Epoch #")
+        plt.ylabel("Loss/Accuracy")
+        plt.legend(loc="lower left")
+        plt.savefig(args["plot"])
+    
+    
+    finally:
+        # save the model to disk
+        print("[INFO] serializing network...")
+        model.save(args["model"])
+        model.save_weights('my_model_weights.h5')
+
+
+
+# python train.py --dataset_train ./data/train --dataset_test ./data/test --model traffic_sign.model
+if __name__ == '__main__':
+    args = args_parse()
+    # construct the image generator for data augmentation
+    train(args)
